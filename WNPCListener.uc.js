a8b14a0a9235c0fd9b03fe8e124da996a1b43f02
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-06-24 00:22:41 -0700

    updates for youtube, small refactor

--- WNPCListener.uc.js
+++ WNPCListener.uc.js
@@ -1,40 +1,40 @@
+// ==UserScript==
+// @name           WebNowPlaying Companion Listener
+// @version        1.1
+// @author         aminomancer
+// @homepage       https://github.com/aminomancer/uc.css.js
+// @description    Listen for updates from the WebNowPlaying Companion Addon, and set background timeout prefs based on whether a video is currently playing or not. If a video is playing, the min timeout values are minimized so that status updates will be sent from the addon to rainmeter as quickly as possible. This way there isn't a lag in the rainmeter skin's pause status after pausing a video. If a video is not playing, min timeout values are increased to conserve computational resources and reduce power consumption.
+// ==/UserScript==
+
 (() => {
+    const prefs = [
+        "dom.min_background_timeout_value",
+        "dom.min_background_timeout_value_without_budget_throttling",
+    ];
+
     function wInit() {
-        const options = {
-                attributes: true,
-                attributeFilter: ["label"],
-            },
-            observer = new MutationObserver(prefToggle);
-        var node = CustomizableUI.getWidget("wnpc_aminomancer-browser-action").forWindow(window)
-            .node;
-
         function xpSet(int) {
-            Services.prefs.setIntPref("dom.min_background_timeout_value", int),
-                Services.prefs.setIntPref(
-                    "dom.min_background_timeout_value_without_budget_throttling",
-                    int
-                );
+            prefs.forEach((pref) => Services.prefs.setIntPref(pref, int));
         }
-
         function xpGet() {
             return Services.prefs.getIntPref("dom.min_background_timeout_value");
         }
-
-        function prefToggle(mutationsList) {
-            for (let mutation of mutationsList) {
-                mutation.target.label === "Connected to media"
+        xpSet(2000);
+        new MutationObserver((mus) => {
+            for (let mu of mus)
+                mu.target.label === "Connected to media"
                     ? xpGet() == 50 || xpSet(50)
                     : xpGet() == 2000 || xpSet(2000);
+        }).observe(
+            CustomizableUI.getWidget("wnpc_aminomancer-browser-action").forWindow(window).node,
+            {
+                attributeFilter: ["label"],
             }
-        }
-
-        xpSet(2000);
-        observer.observe(node, options);
+        );
     }
 
-    if (gBrowserInit.delayedStartupFinished) {
-        setTimeout(wInit, 5000);
-    } else {
+    if (gBrowserInit.delayedStartupFinished) setTimeout(wInit, 5000);
+    else {
         let delayedListener = (subject, topic) => {
             if (topic == "browser-delayed-startup-finished" && subject == window) {
                 Services.obs.removeObserver(delayedListener, topic);
--- Websites/YoutubeNew.js
+++ Websites/YoutubeNew.js
@@ -16,23 +16,28 @@ function fancyTimeFormat(dur) {
 
     // Output like "1:01" or "4:03:59" or "123:03:59"
     let ret = "";
-
-    if (days > 0) {
-        ret += "" + days + ":" + (hrs < 10 ? "0" : "");
-    }
-
-    if (hrs > 0) {
-        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-    }
-
-    if (mins > 0) {
-        ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-    } else {
-        ret += "" + 0 + ":";
-    }
-
+    if (days > 0) ret += "" + days + ":" + (hrs < 10 ? "0" : "");
+    if (hrs > 0) ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    if (mins > 0) ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    else ret += "" + 0 + ":";
     ret += "" + secs;
     return ret;
+}
+
+// click thumbs up, thumbs down, share, or save = pass 0, 1, 2, 3
+function clickTopLevelButton(menu, i) {
+    let topLevelButtons = menu.querySelector("#top-level-buttons");
+    if (!topLevelButtons || topLevelButtons.hidden)
+        topLevelButtons = menu.querySelector("#top-level-buttons-computed");
+    topLevelButtons.children[i]?.click();
+}
+
+// check thumbs up, thumbs down, share, or save = pass 0, 1, 2, 3
+function checkTopLevelButton(menu, i) {
+    let topLevelButtons = menu.querySelector("#top-level-buttons");
+    if (!topLevelButtons || topLevelButtons.hidden)
+        topLevelButtons = menu.querySelector("#top-level-buttons-computed");
+    return topLevelButtons.children[i]?.classList.contains("style-default-active");
 }
 
 function setupNew() {
@@ -53,13 +58,10 @@ function setupNew() {
         if (
             document.getElementsByClassName("ytp-play-button")[0]?.getAttribute("aria-label") ===
             null
-        ) {
+        )
             state = 3;
-        }
         //It is possible for the video to be "playing" but not started
-        if (state == 1 && video.played.length <= 0) {
-            state = 2;
-        }
+        if (state == 1 && video.played.length <= 0) state = 2;
         return state;
     };
     youtubeInfoHandler.title = function () {
@@ -73,9 +75,7 @@ function setupNew() {
     youtubeInfoHandler.album = function () {
         //If using a playlist just use the title of that
         let playlist = document.getElementsByClassName("ytd-playlist-panel-renderer title")[0];
-        if (playlist?.innerText !== "") {
-            return playlist.innerText;
-        }
+        if (playlist?.innerText !== "") return playlist.innerText;
 
         //If video has a "Buy or Rent" module use the displayed title & year
         let offer = document.getElementById("offer-module");
@@ -102,9 +102,7 @@ function setupNew() {
                         year?.innerText?.length > 0
                             ? `${title?.innerText} (${year?.innerText})`
                             : title?.innerText;
-                } else {
-                    currCategory = title?.innerText;
-                }
+                } else currCategory = title?.innerText;
                 return currCategory;
             }
         }
@@ -122,20 +120,13 @@ function setupNew() {
             //Return category if visible else
             try {
                 let titles = info2nd?.querySelectorAll("#title"),
-                    subtitles = info2nd?.querySelectorAll("#subtitle"),
-                    title = titles[0],
-                    subtitle = subtitles[0],
-                    category = titles[1],
-                    catsub = subtitles[1];
-                if (titles[0]?.hidden == false && subtitles[0]?.hidden == false) {
+                    subtitles = info2nd?.querySelectorAll("#subtitle");
+                if (titles[0]?.hidden == false && subtitles[0]?.hidden == false)
                     currCategory = `${titles[0]?.innerText} (${subtitles[0]?.innerText})`;
-                } else if (titles[0]?.hidden == false) {
-                    currCategory = titles[0]?.innerText;
-                } else if (titles[1]?.hidden == false && subtitles[1]?.hidden == false) {
+                else if (titles[0]?.hidden == false) currCategory = titles[0]?.innerText;
+                else if (titles[1]?.hidden == false && subtitles[1]?.hidden == false)
                     currCategory = `${titles[1]?.innerText} (${subtitles[1]?.innerText})`;
-                } else if (titles[1]?.hidden == false) {
-                    currCategory = titles[1]?.innerText;
-                }
+                else if (titles[1]?.hidden == false) currCategory = titles[1]?.innerText;
                 return currCategory;
             } catch (e) {
                 return currCategory;
@@ -155,11 +146,8 @@ function setupNew() {
                 qual = "/maxresdefault.jpg?";
             img.setAttribute("src", strr + videoID + qual);
             img.addEventListener("load", function () {
-                if (img.height > 90) {
-                    currIMG = strr + videoID + qual;
-                } else {
-                    currIMG = strr + videoID + "/hqdefault.jpg?";
-                }
+                if (img.height > 90) currIMG = strr + videoID + qual;
+                else currIMG = strr + videoID + "/hqdefault.jpg?";
             });
             img.addEventListener("error", function () {
                 if (img.src.includes("maxresdefault")) {
@@ -191,40 +179,22 @@ function setupNew() {
         return document.getElementsByClassName("html5-main-video")[0].volume;
     };
     youtubeInfoHandler.rating = function () {
-        let buttons = document
-            .getElementById("menu-container")
-            ?.getElementsByTagName("ytd-toggle-button-renderer");
+        let menu = document.getElementById("menu-container");
         //Check if thumbs button is active
-        if (buttons[0]?.classList.contains("style-default-active")) {
-            return 5; // thumbs up
-        }
-        if (buttons[1]?.classList.contains("style-default-active")) {
-            return 1; // thumbs down
-        }
+        if (checkTopLevelButton(menu, 0)) return 5; // thumbs up
+        if (checkTopLevelButton(menu, 1)) return 1; // thumbs down
         return 0;
     };
     youtubeInfoHandler.repeat = function () {
-        if (document.getElementsByClassName("html5-main-video")[0].loop) {
-            return 2;
-        }
+        if (document.getElementsByClassName("html5-main-video")[0].loop) return 2;
         let menu = document.getElementById("playlist-action-menu");
-        if (menu.children?.length > 0) {
-            return menu
-                .getElementsByTagName("ytd-toggle-button-renderer")[0]
-                .classList.contains("style-default-active")
-                ? 1
-                : 0;
-        }
+        if (menu.children?.length > 0) return checkTopLevelButton(menu, 0) ? 1 : 0;
         return 0;
     };
     youtubeInfoHandler.shuffle = function () {
         let menu = document.getElementById("playlist-action-menu");
         if (menu.children.length > 0) {
-            return menu
-                .getElementsByTagName("ytd-toggle-button-renderer")[1]
-                ?.classList.contains("style-default-active")
-                ? 1
-                : 0;
+            return checkTopLevelButton(menu, 1) ? 1 : 0;
         }
         return 0;
     };
@@ -241,106 +211,68 @@ function setupNew() {
     youtubeEventHandler.next = function () {
         let next = document.getElementsByClassName("ytp-next-button")[0],
             playlist = document.getElementsByClassName("playlist-items")[0];
-        if (!document.getElementById("playlist")?.hasAttribute("has-playlist-buttons")) {
+        if (!document.getElementById("playlist")?.hasAttribute("has-playlist-buttons"))
             next.click();
-        } else if (currShuffle == 1) {
+        else if (currShuffle == 1)
             playlist.children[Math.floor(Math.random() * playlist.children.length)]
                 .querySelector("#meta")
                 ?.click();
-        } else {
-            if (!playlist.lastElementChild?.hasAttribute("selected")) {
-                playlist
-                    .querySelector("#playlist-items[selected]")
-                    ?.nextSibling?.querySelector("#meta")
-                    ?.click();
-            } else {
-                if (
-                    document
-                        .getElementById("playlist-action-menu")
-                        ?.getElementsByTagName("ytd-toggle-button-renderer")[0]
-                        ?.classList.contains("style-default-active")
-                ) {
-                    playlist.firstElementChild.querySelector("#meta").click();
-                } else {
-                    next.click();
-                }
-            }
-        }
+        else if (!playlist.lastElementChild?.hasAttribute("selected"))
+            playlist
+                .querySelector("#playlist-items[selected]")
+                ?.nextSibling?.querySelector("#meta")
+                ?.click();
+        else if (checkTopLevelButton(document.getElementById("playlist-action-menu"), 0))
+            playlist.firstElementChild.querySelector("#meta").click();
+        else next.click();
     };
     youtubeEventHandler.previous = function () {
         let video = document.getElementsByClassName("html5-main-video")[0],
             previous = document.getElementsByClassName("ytp-prev-button")[0];
-        if (previous?.getAttribute("aria-disabled") == "false") {
-            previous.click();
-        } else {
-            if (video.currentTime <= 3) {
-                history.back();
-            } else {
-                video.currentTime = 0;
-            }
-        }
+        if (previous?.getAttribute("aria-disabled") == "false") previous.click();
+        else if (video.currentTime <= 3) history.back();
+        else video.currentTime = 0;
     };
     youtubeEventHandler.progressSeconds = function (position) {
         document.getElementsByClassName("html5-main-video")[0].currentTime = position;
     };
     youtubeEventHandler.volume = function (volume) {
         let video = document.getElementsByClassName("html5-main-video")[0];
-        if (video.muted && volume > 0) {
-            video.muted = false;
-        } else if (volume == 0) {
-            video.muted = true;
-        }
+        video.muted = volume == 0;
         video.volume = volume;
     };
     youtubeEventHandler.repeat = function () {
         let video = document.getElementsByClassName("html5-main-video")[0];
+        let menu = document.getElementById("playlist-action-menu");
         //If no repeat button on the page then use video's loop element to loop the video
-        if (!document.getElementById("playlist")?.hasAttribute("has-playlist-buttons")) {
+        if (
+            !document.getElementById("playlist")?.hasAttribute("has-playlist-buttons") ||
+            !menu.children.length
+        )
             video.loop = !video.loop;
-        } else {
-            let repeatButton = document
-                .getElementById("playlist-action-menu")
-                .getElementsByTagName("ytd-toggle-button-renderer")[0];
+        else {
             //Each if is a different state, first is loop none, second is loop one, last is loop all order triggered is still the usual none->all->one
             if (video.loop) {
                 video.loop = false;
-                if (repeatButton?.classList.contains("style-default-active")) {
-                    repeatButton.click();
-                }
-            } else if (repeatButton?.classList.contains("style-default-active")) {
-                video.loop = true;
-            } else {
-                repeatButton?.click();
-            }
+                if (checkTopLevelButton(menu, 0)) clickTopLevelButton(menu, 0);
+            } else if (checkTopLevelButton(menu, 0)) video.loop = true;
+            else clickTopLevelButton(menu, 0);
         }
     };
     youtubeEventHandler.shuffle = function () {
-        let menu = document.getElementById("playlist-action-menu"),
-            shuffleButton = menu?.getElementsByTagName("ytd-toggle-button-renderer")[1];
-        if (menu?.children.length > 0) {
-            shuffleButton?.click();
-        }
+        let menu = document.getElementById("playlist-action-menu");
+        if (menu?.children.length > 0) clickTopLevelButton(menu, 1);
     };
     youtubeEventHandler.toggleThumbsUp = function () {
-        document
-            .getElementById("menu-container")
-            ?.getElementsByTagName("ytd-toggle-button-renderer")[0]
-            ?.click();
+        let menu = document.getElementById("menu-container");
+        clickTopLevelButton(menu, 0);
     };
     youtubeEventHandler.toggleThumbsDown = function () {
-        document
-            .getElementById("menu-container")
-            ?.getElementsByTagName("ytd-toggle-button-renderer")[1]
-            ?.click();
+        let menu = document.getElementById("menu-container");
+        clickTopLevelButton(menu, 1);
     };
     youtubeEventHandler.rating = function (rating) {
-        let buttons = document
-            .getElementById("menu-container")
-            ?.getElementsByTagName("ytd-toggle-button-renderer");
-        if (rating > 3) {
-            buttons[0]?.click();
-        } else if (rating < 3) {
-            buttons[1]?.click();
-        }
+        let menu = document.getElementById("menu-container");
+        clickTopLevelButton(menu, rating < 3 ? 1 : 0);
     };
 }
