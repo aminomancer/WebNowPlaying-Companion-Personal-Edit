689a645166d30779baf866a7a572dea87ee72fd3
aminomancer <33384265+aminomancer@users.noreply.github.com>
2021-12-26 21:19:17 -0800

    0.9.4: bring back netflix
    Figured out the Netflix site. The issue was x-ray vision working differently on a random assortment of react objects recursively referenced as properties on other react objects. So now the netflix handler should work on Firefox, not sure about Chrome.

--- Websites/Netflix.js
+++ Websites/Netflix.js
@@ -2,7 +2,10 @@
 /*global init createNewMusicInfo createNewMusicEventHandler convertTimeToString capitalize*/
 
 function getContext() {
-    return window.wrappedJSObject.netflix.appContext;
+    let win;
+    if ("wrappedJSObject" in window) win = window.wrappedJSObject;
+    else win = window;
+    return win.netflix.appContext;
 }
 
 function findReact(dom) {
@@ -12,27 +15,44 @@ function findReact(dom) {
 
 function getReactFromSelector(selector) {
     const elems = document.querySelectorAll(selector);
-    return elems.length > 0 ? findReact(elems[0]) : null;
+    if (elems.length > 0) {
+        let elem;
+        if ("wrappedJSObject" in elems[0]) elem = elems[0].wrappedJSObject;
+        else elem = elems[0];
+        return findReact(elem);
+    } else return null;
 }
 
 function getRepo(context) {
     return context.getPlayerApp()?.getState()?.videoPlayer?.cadmiumPlayerRepository;
 }
 
+function getSessionId() {
+    const react = getReactFromSelector(`.watch-video--player-view`);
+    return react?.memoizedProps?.sessionId;
+}
+
+function getAPI() {
+    return getContext()?.getState().playerApp.getAPI();
+}
+
+function getActionCreators() {
+    return getContext()?.getPlayerApp().getActionCreators();
+}
+
 function getPlayer() {
     const appContext = getContext();
     if (appContext) {
         const cadmium = getRepo(appContext);
-        const sessionId =
-            getReactFromSelector(`.watch-video--player-view`)?.memoizedProps?.sessionId;
-        return cadmium.getPlayer(sessionId);
+        const player = cadmium.getPlayer(getSessionId());
+        return player;
     } else return null;
 }
 
 function getMetadata() {
     try {
         return Object.values(
-            getContext()?.state.playerApp.getState()?.videoPlayer.videoMetadata
+            getContext()?.getPlayerApp().getState()?.videoPlayer.videoMetadata
         ).find((data) => "_video" in data);
     } catch (e) {
         return null;
@@ -44,8 +64,8 @@ function getSeasonData() {
     if (metadata) {
         let { seasons } = metadata;
         let getEpisode = (season) =>
-            season.episodes.find((episode) => episode.id === metadata.currentEpisode);
-        let season = seasons.find(getEpisode);
+            [...season.episodes].find((episode) => episode.id === metadata.currentEpisode);
+        let season = [...seasons].find(getEpisode);
         return {
             type: metadata.type,
             title: metadata.title,
@@ -53,6 +73,26 @@ function getSeasonData() {
             season,
             seasons,
         };
+    }
+}
+
+function getNavData() {
+    let data = getSeasonData();
+    if (data) {
+        let { episodes, seq } = data.season;
+        let seasons = [...data.seasons];
+        let eIndex = [...episodes].findIndex((episode) => episode.id === data.episode.id);
+        let currId = [...episodes][eIndex].id;
+        let prevId, nextId;
+        if (eIndex > 0) prevId = [...episodes][eIndex - 1]?.id;
+        else if (seq > 1) {
+            let prevEpisodes = [...seasons[seq - 2]?.episodes];
+            prevId = prevEpisodes[prevEpisodes.length - 1]?.id;
+        }
+        if (eIndex === episodes.length - 1 && seq < seasons.length)
+            nextId = [...seasons[seq]?.episodes][0]?.id;
+        else nextId = [...episodes][eIndex + 1]?.id;
+        return { prevId, currId, nextId };
     }
 }
 
@@ -77,20 +117,20 @@ function setup() {
         if (data)
             if (data.type === "show" && data.episode?.title) {
                 let { title } = data.episode;
-                if (data.season && data.season.episodes.length > 1 && data.season.shortName)
+                if (data.season && [...data.season.episodes].length > 1 && data.season.shortName)
                     title += ` (${data.season.shortName}E${data.episode.seq})`;
-                return title;
-            } else return data.title;
+                return String(title);
+            } else return String(data.title);
     };
     netflixInfoHandler.artist = function () {
         let data = getSeasonData();
-        if (data && data.type === "show" && data.episode?.title) return data.title;
+        if (data && data.type === "show" && data.episode?.title) return String(data.title);
         else return "Netflix";
     };
     netflixInfoHandler.album = function () {
         let data = getSeasonData();
         if (data && data.type === "show" && data.episode?.title && data.season.longName)
-            return data.season.longName;
+            return String(data.season.longName);
         else return "Netflix";
     };
     netflixInfoHandler.cover = function () {
@@ -99,10 +139,10 @@ function setup() {
         if (artwork?.length > 0) art = artwork;
         else if (storyart?.length > 0) art = storyart;
         else art = boxart;
-        if (art?.length > 0)
-            return art.reduce((prev, cur) => {
-                return prev.w * prev.h > cur.w * cur.h ? prev : cur;
-            })?.url;
+        art = [...art].reduce((prev, cur) => {
+            return prev.w * prev.h > cur.w * cur.h ? prev : cur;
+        });
+        return String(art?.url) || "";
     };
     netflixInfoHandler.duration = function () {
         return getPlayer().getDuration() / 1000;
@@ -114,9 +154,9 @@ function setup() {
         return getPlayer().getVolume();
     };
     netflixInfoHandler.rating = function () {
-        let { userRating } = getMetadata()?._metadata.video;
+        let { userRating } = getMetadata()?._metadata.video.userRating;
         if (userRating === 2) return 5;
-        else return userRating;
+        else return Number(userRating || 0);
     };
     netflixInfoHandler.repeat = null;
     netflixInfoHandler.shuffle = null;
@@ -131,27 +171,47 @@ function setup() {
         player.getPaused() ? player.play() : player.pause();
     };
     netflixEventHandler.next = function () {
-        let player = getPlayer();
-        player.seek(player.getDuration());
+        let data = getNavData();
+        if (data?.nextId && data?.currId)
+            document.location.href = document.location.href.replace(data.currId, data.nextId);
     };
     netflixEventHandler.previous = function () {
-        let player = getPlayer();
-        player.seek(0);
+        let data = getNavData();
+        if (data?.prevId && data?.currId)
+            document.location.href = document.location.href.replace(data.currId, data.prevId);
     };
     netflixEventHandler.progressSeconds = function (position) {
-        let player = getPlayer();
-        player.seek(position);
-        // player.seek(player.getCurrentTime() + position);
+        getPlayer().seek(position * 1000);
     };
     netflixEventHandler.volume = function (volume) {
-        let player = getPlayer();
-        player.setVolume(volume);
+        getPlayer().setVolume(volume);
     };
     netflixEventHandler.repeat = null;
     netflixEventHandler.shuffle = null;
     netflixEventHandler.toggleThumbsUp = null;
     netflixEventHandler.toggleThumbsDown = null;
     netflixEventHandler.rating = null;
+    // netflixEventHandler.next = function () {
+    //     // the simplest way to do this, just go to the very end,
+    //     // whereupon netflix will automatically progress to the next episode.
+    //     let player = getPlayer();
+    //     player.seek(player.getDuration());
+    // };
+    // netflixEventHandler.previous = function () {
+    //     // go back to the beginning of the video. won't actually change titles.
+    //     let player = getPlayer();
+    //     player.seek(0);
+    // };
+    // netflixEventHandler.next = function () {
+    //     // use netflix's internal API to play the next episode.
+    //     // there's no equivalent method to play the previous episode,
+    //     // and the internal methods for playing an arbitrary episode are really hard to figure out.
+    //     // more importantly, after calling this, netflix doesn't do anything for like 3-4 seconds for some reason.
+    //     // so you're just sitting around waiting and you might even wind up hitting it twice and screwing everything up.
+    //     // so I'm just doing all of this manually even though it's kinda nasty.
+    //     let playbackState = getAPI().getPlaybackStateBySessionId(getSessionId());
+    //     getActionCreators().playerPlayNextVideo(playbackState);
+    // };
 }
 
 setup();
