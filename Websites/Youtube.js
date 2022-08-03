07fe546e87343c16132e74829f0ceba6465f1c7b
aminomancer <33384265+aminomancer@users.noreply.github.com>
2022-08-03 03:57:04 -0700

    0.9.7: use a more reliable method of searching for top-level buttons to check/click. should eliminate the errors on queue lists, etc.

--- Websites/Youtube.js
+++ Websites/Youtube.js
@@ -7,6 +7,7 @@ var currImg = "";
 var currCategory = "";
 var wasMadeVisable = false;
 
+// A fallback in case the video's time display isn't working.
 function fancyTimeFormat(dur) {
   // Hours, minutes and seconds
   let days = ~~(dur / 86400);
@@ -28,48 +29,132 @@ function isMiniPlayerActive() {
   return document.querySelector("ytd-miniplayer").active;
 }
 
+// There's a main container and a miniplayer container. We usually want to
+// search for elements in whichever one is active, so use this instead of
+// document wherever possible.
 function getContainer() {
   let selector = isMiniPlayerActive() ? "ytd-miniplayer" : "body > ytd-app > #content";
   return document.querySelector(selector);
 }
 
+/**
+ * There's a main container and a miniplayer container. We usually want to
+ * search for elements in whichever one is active, so use this instead of
+ * document.querySelector(selector) wherever possible.
+ * @param {String} selector A CSS selector string
+ * @returns {DOMNode|undefined} Just like document.querySelector
+ */
 function findContainerElement(selector) {
   return getContainer().querySelector(selector) || document.querySelector(selector);
 }
 
+// Synchronously retrieve info about the video without parsing DOM.
 function getVideoDetails() {
-  return (
-    document.querySelector("ytd-app").wrappedJSObject?.data?.playerResponse?.videoDetails ?? {}
-  );
+  let app = document
+    .querySelector("ytd-app")
+    .wrappedJSObject?.get("data.playerResponse.videoDetails");
+  XPCNativeWrapper(app);
+  return app ?? {};
 }
 
-function getPlaylist() {
-  return getContainer().querySelector("#playlist").wrappedJSObject?.data ?? {};
+// Synchronously retrieve info about the playlist without parsing DOM.
+function getPlaylistDetails() {
+  let playlist = getContainer().querySelector("#playlist").wrappedJSObject?.get("data");
+  XPCNativeWrapper(playlist);
+  return playlist ?? {};
 }
 
-// click a menu button i.e. thumbs up/down, loop, shuffle.
-// menu narrows the check, i is the DOM index of the desired button
-function clickTopLevelButton(menu, i) {
-  if (!menu) return;
-  let topLevelButtons = menu.querySelector("#top-level-buttons");
-  if (!topLevelButtons || topLevelButtons.hidden)
-    topLevelButtons = menu.querySelector("#top-level-buttons-computed");
-  let renderer = topLevelButtons?.children[i];
-  if (typeof renderer.wrappedJSObject.button === "object") renderer.wrappedJSObject.button.click();
-  else renderer.click();
+/**
+ * Click a menu button, i.e. thumbs up/down, loop, shuffle. Must be a button
+ * contained in #top-level-buttons or #top-level-buttons-computed.
+ * @param {DOMNode} menu The container to search for the button in. Should be
+ *   the parent of a ytd-menu-renderer element.
+ * @param {String} query A data property to search for. This is basically a
+ *   selector for a JS property instead of for an HTML attribute. It's needed
+ *   because the buttons don't have any identifying HTML attributes. We'd be
+ *   forced to just target them by child index otherwise, but such indices
+ *   aren't consistent. An example is "data.targetId" which you can see inside
+ *   the top level button in the console at button.__data.data.targetId
+ * @param {*} [val] The value the data property specified by query should have.
+ *   This is optional. If omitted, we'll accept the button regardless of the
+ *   property value, as long as it has the property. This is usually a string.
+ *   An example matching the above query parameter is "watch-like"
+ */
+function clickTopLevelButton(menu, { query, val } = {}) {
+  if (!menu || !query) return false;
+  let buttons = menu.querySelector("#top-level-buttons");
+  if (!buttons || buttons.hidden) buttons = menu.querySelector("#top-level-buttons-computed");
+  if (!buttons) return;
+
+  buttons = [...buttons.children];
+  let button = buttons.find(btn => {
+    let found = btn.wrappedJSObject?.get(query);
+    XPCNativeWrapper(found);
+    if (val) return found === val;
+    else return !!found;
+  });
+  if (!button) return;
+
+  let innerButton = button.wrappedJSObject?.get("button");
+  XPCNativeWrapper(innerButton);
+  if (typeof innerButton === "object") innerButton.click();
+  else button.click();
 }
 
-// check active state for a menu button i.e. thumbs up/down, loop, shuffle.
-// menu narrows the check, i is the DOM index of the desired button
-function checkTopLevelButton(menu, i) {
-  let topLevelButtons = menu.querySelector("#top-level-buttons");
-  if (!topLevelButtons || topLevelButtons.hidden)
-    topLevelButtons = menu.querySelector("#top-level-buttons-computed");
-  let renderer = topLevelButtons?.children[i];
-  let states = ["PLAYLIST_LOOP_STATE_NONE", "PLAYLIST_LOOP_STATE_ALL", "PLAYLIST_LOOP_STATE_ONE"];
-  let currentState = renderer.wrappedJSObject.get("currentState");
-  if (states && currentState) return Math.max(states.indexOf(currentState), 0);
-  return renderer.classList.contains("style-default-active");
+/**
+ * Check a menu button's active state. Must be a button contained in
+ * #top-level-buttons or #top-level-buttons-computed.
+ * @param {DOMNode} menu The container to search for the button in. Should be
+ *   the parent of a ytd-menu-renderer element.
+ * @param {String} query A data property to search for. This is basically a
+ *   selector for a JS property instead of for an HTML attribute. It's needed
+ *   because the buttons don't have any identifying HTML attributes. We'd be
+ *   forced to just target them by child index otherwise, but such indices
+ *   aren't consistent. An example is "data.targetId" which you can see inside
+ *   the top level button in the console at button.__data.data.targetId
+ * @param {*} [val] The value the data property specified by query should have.
+ *   This is optional. If omitted, we'll accept the button regardless of the
+ *   property value, as long as it has the property. This is usually a string.
+ *   An example matching the above query parameter is "watch-like"
+ * @returns {Boolean|Number|null} If the button is a toggle button, return true
+ *   if the button is toggled on. If it's a cycle button like the repeat/loop
+ *   button, return an integer representing the index of the button's current
+ *   state relative to its possible states. These states match the Rainmeter
+ *   plugin's repeat states: 0 is loop off, 1 is loop all, 2 is loop one.
+ *   Finally, if this method doesn't support the button passed, return null.
+ */
+function checkTopLevelButton(menu, { query, val } = {}) {
+  if (!menu || !query) return null;
+  let buttons = menu.querySelector("#top-level-buttons");
+  if (!buttons || buttons.hidden) buttons = menu.querySelector("#top-level-buttons-computed");
+  if (!buttons) return null;
+
+  buttons = [...buttons.children];
+  let button = buttons.find(btn => {
+    let found = btn.wrappedJSObject?.get(query);
+    XPCNativeWrapper(found);
+    if (val) return found === val;
+    else return !!found;
+  });
+  if (!button) return null;
+
+  let data = button.wrappedJSObject?.get("data");
+  XPCNativeWrapper(data);
+  if (data && data.states) {
+    let states = [...data.states]?.map(state => {
+      for (let prop in state) {
+        if (typeof state[prop] === "object" && "state" in state[prop]) return state[prop].state;
+      }
+    });
+    if (states) {
+      // loop states = ["PLAYLIST_LOOP_STATE_NONE", "PLAYLIST_LOOP_STATE_ALL", "PLAYLIST_LOOP_STATE_ONE"];
+      let currentState = button.wrappedJSObject?.get("currentState");
+      XPCNativeWrapper(currentState);
+      if (currentState) return Math.max(states.indexOf(currentState), 0);
+    }
+  }
+
+  return button.classList.contains("style-default-active");
 }
 
 function setup() {
@@ -78,10 +163,12 @@ function setup() {
   youtubeInfoHandler.player = function () {
     return "YouTube";
   };
+
   youtubeInfoHandler.readyCheck = function () {
     let title = findContainerElement("h1.ytd-video-primary-info-renderer.title");
     return title && title.innerText?.length > 0;
   };
+
   youtubeInfoHandler.state = function () {
     let video = document.querySelector(".html5-main-video");
     let state = video.paused ? 2 : 1;
@@ -92,19 +179,22 @@ function setup() {
     if (state == 1 && video.played.length <= 0) state = 2;
     return state;
   };
+
   youtubeInfoHandler.title = function () {
     let { title } = getVideoDetails();
     if (title) return title;
     return findContainerElement("h1.ytd-video-primary-info-renderer.title")?.innerText;
   };
+
   youtubeInfoHandler.artist = function () {
     let { author } = getVideoDetails();
     if (author) return author;
     return findContainerElement("#upload-info yt-formatted-string.ytd-channel-name")?.innerText;
   };
+
   youtubeInfoHandler.album = function () {
     // If using a playlist just use the title of that
-    let { title } = getPlaylist();
+    let { title } = getPlaylistDetails();
     if (title?.length) return title;
     let playlist = findContainerElement(".ytd-playlist-panel-renderer.title");
     if (playlist?.innerText !== "") return playlist.innerText;
@@ -163,26 +253,37 @@ function setup() {
     // Return no album/last category
     return currCategory;
   };
+
   youtubeInfoHandler.cover = function () {
-    let { videoId, thumbnail } = getVideoDetails();
+    let { videoId } = getVideoDetails();
     if (!videoId) videoId = new URLSearchParams(new URL(window.location.href).search).get("v");
-    if (lastImgVideoID !== videoId && videoId) {
+    if (videoId && lastImgVideoID !== videoId) {
       lastImgVideoID = videoId;
-      const thumbnails = [...thumbnail?.thumbnails].sort((a, b) => a.height < b.height);
-      let img = document.createElement("img");
-      let idx = 0;
-      img.setAttribute("src", `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`);
-      img.addEventListener("load", () => {
-        currImg = img.src;
+      const strr = "https://i.ytimg.com/vi/";
+      let img = document.createElement("img"),
+        qual = "/maxresdefault.jpg";
+      img.setAttribute("src", strr + videoId + qual);
+      img.addEventListener("load", function () {
+        if (img.height > 90) currIMG = strr + videoId + qual;
+        else currIMG = strr + videoId + "/hqdefault.jpg?";
       });
-      img.addEventListener("error", () => {
-        let newImg = thumbnails[idx++]?.url;
-        if (newImg) img.setAttribute("src", newImg);
-        else img.remove();
+      img.addEventListener("error", function () {
+        if (img.src.includes("maxresdefault")) {
+          qual = "/hqdefault.jpg";
+          currIMG = strr + videoId + qual;
+        } else if (img.src.includes("hqdefault")) {
+          qual = "/mqdefault.jpg";
+          currIMG = strr + videoId + qual;
+        } else if (img.src.includes("mqdefault")) {
+          qual = "/maxresdefault.jpg";
+          currIMG = strr + lastImgVideoID + qual;
+        }
+        img.setAttribute("src", strr + videoId + qual);
       });
     }
-    return currImg;
+    return currIMG;
   };
+
   youtubeInfoHandler.durationString = function () {
     return (
       document.querySelector(
@@ -190,38 +291,49 @@ function setup() {
       )?.innerText || fancyTimeFormat(document.querySelector(".html5-main-video").duration)
     );
   };
+
   youtubeInfoHandler.position = function () {
     return document.querySelector(".html5-main-video").currentTime;
   };
+
   youtubeInfoHandler.volume = function () {
     return document.querySelector(".html5-main-video").volume;
   };
+
   youtubeInfoHandler.rating = function () {
     let menu = findContainerElement("#menu-container");
-    // Check if thumbs button is active
-    if (checkTopLevelButton(menu, 0)) return 5; // thumbs up
-    if (checkTopLevelButton(menu, 1)) return 1; // thumbs down
+    // Thumbs up
+    if (checkTopLevelButton(menu, { query: "data.targetId", val: "watch-like" })) return 5;
+    // Thumbs down
+    if (checkTopLevelButton(menu, { query: "data.targetId", val: "watch-dislike" })) return 1;
     return 0;
   };
+
   youtubeInfoHandler.repeat = function () {
     if (document.querySelector(".html5-main-video").loop) return 2;
     let menu = findContainerElement("#playlist-action-menu");
-    if (menu?.children.length > 0) return Number(checkTopLevelButton(menu, 0));
+    if (menu?.children.length > 0) {
+      return Number(checkTopLevelButton(menu, { query: "playlistLoopStateEntity" }));
+    }
     return 0;
   };
+
   youtubeInfoHandler.shuffle = function () {
     let menu = findContainerElement("#playlist-action-menu");
-    if (menu?.children.length > 0) return Number(checkTopLevelButton(menu, 1));
+    if (menu?.children.length > 0) {
+      return Number(checkTopLevelButton(menu, { query: "buttonIcon", val: "yt-icons:shuffle" }));
+    }
     return 0;
   };
 
   var youtubeEventHandler = createNewMusicEventHandler();
 
-  // Define custom check logic to make sure you are not trying to update info when nothing is playing
   youtubeEventHandler.readyCheck = null;
+
   youtubeEventHandler.playpause = function () {
     findContainerElement(".ytp-play-button")?.click();
   };
+
   youtubeEventHandler.next = function () {
     let next = findContainerElement(".ytp-next-button");
     let playlist = findContainerElement(".playlist-items");
@@ -239,6 +351,7 @@ function setup() {
       playlist.firstElementChild.querySelector("#meta").click();
     } else next.click();
   };
+
   youtubeEventHandler.previous = function () {
     let video = document.querySelector(".html5-main-video");
     let previous = findContainerElement(".ytp-prev-button");
@@ -247,23 +360,26 @@ function setup() {
     else if (video.currentTime <= 3) history.back();
     else video.currentTime = 0;
   };
+
   youtubeEventHandler.progressSeconds = function (position) {
     document.querySelector(".html5-main-video").currentTime = position;
   };
+
   youtubeEventHandler.volume = function (volume) {
     let video = document.querySelector(".html5-main-video");
     video.muted = volume == 0;
     video.volume = volume;
   };
+
   youtubeEventHandler.repeat = function () {
     let video = document.querySelector(".html5-main-video");
     let menu = findContainerElement("#playlist-action-menu");
-    if (
-      findContainerElement("#playlist")?.wrappedJSObject.hasPlaylistButtons &&
-      menu.children.length > 0
-    ) {
-      let state = checkTopLevelButton(menu, 0);
-      let click = () => clickTopLevelButton(menu, 0);
+    let hasPlaylistButtons =
+      findContainerElement("#playlist")?.wrappedJSObject?.get("hasPlaylistButtons");
+    if (hasPlaylistButtons && menu.children.length > 0) {
+      XPCNativeWrapper(hasPlaylistButtons);
+      let state = checkTopLevelButton(menu, { query: "playlistLoopStateEntity" });
+      let click = () => clickTopLevelButton(menu, { query: "playlistLoopStateEntity" });
       // If the new repeat button is enabled, use that. It allows cycling
       // between no loop, loop playlist, and loop one (single video).
       if (typeof state === "number") {
@@ -281,17 +397,32 @@ function setup() {
     // property to loop the video
     else video.loop = !video.loop;
   };
+
   youtubeEventHandler.shuffle = function () {
     let menu = findContainerElement("#playlist-action-menu");
-    if (menu?.children.length > 0) clickTopLevelButton(menu, 1);
+    if (menu?.children.length > 0) {
+      clickTopLevelButton(menu, { query: "buttonIcon", val: "yt-icons:shuffle" });
+    }
   };
+
   youtubeEventHandler.toggleThumbsUp = function () {
-    clickTopLevelButton(findContainerElement("#menu-container"), 0);
+    clickTopLevelButton(findContainerElement("#menu-container"), {
+      query: "data.targetId",
+      val: "watch-like",
+    });
   };
+
   youtubeEventHandler.toggleThumbsDown = function () {
-    clickTopLevelButton(findContainerElement("#menu-container"), 1);
+    clickTopLevelButton(findContainerElement("#menu-container"), {
+      query: "data.targetId",
+      val: "watch-dislike",
+    });
   };
+
   youtubeEventHandler.rating = function (rating) {
-    clickTopLevelButton(findContainerElement("#menu-container"), rating < 3 ? 1 : 0);
+    clickTopLevelButton(findContainerElement("#menu-container"), {
+      query: "data.targetId",
+      val: rating < 3 ? "watch-dislike" : "watch-like",
+    });
   };
 }
